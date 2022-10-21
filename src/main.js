2bbb630e5127c7be3040bf82ed06a7757d37658e
GustavWebCriador <ovatsug.luiz@hotmail.com>
2022-10-21 04:08:32 -0300

    Finalizado

--- src/main.js
+++ src/main.js
@@ -1,1 +1,20 @@
 import "./css/index.css"
+
+const ccBgColor01 = document.querySelector(".cc-bg svg > g g:nth-child(1) path")
+const ccBgColor02 = document.querySelector(".cc-bg svg > g g:nth-child(2) path")
+const ccLogo = document.querySelector(".cc-logo span:nth-child(2) img")
+
+function setCardType(type) {
+  const colors = {
+    visa: ["#FFA520", "#004C90"],
+    mastercard: ["#fc9700", "#eb001b"],
+    default: ["black", "gray"],
+  }
+
+  ccBgColor01.setAttribute("fill", colors[type][0])
+  ccBgColor02.setAttribute("fill", colors[type][1])
+  ccLogo.setAttribute("src", `cc-${type}.svg`)
+}
+
+//Deixando função global para ver direto no console
+globalThis.setCardType = setCardType
